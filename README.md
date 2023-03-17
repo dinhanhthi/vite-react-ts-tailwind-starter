164251a6e6099c1a2a106805569a55bcaa3b5328
Thi <thi@ideta.io>
2023-03-17 02:38:07 +0100

    Linter + configuration

---
+++ README.md
@@ -0,0 +1,27 @@
+# ⚡ vite-react-ts-tailwind-starter
+
+A starter to quickly make a playground for a React app.
+
+## ✨ Tech specs
+
+- [Vite](https://vitejs.dev/).
+- [React](https://reactjs.org/).
+- [TypeScript](https://www.typescriptlang.org/).
+- [Tailwind CSS](https://tailwindcss.com/).
+- [React Icons](https://react-icons.github.io/react-icons/).
+- [ESLint](https://eslint.org/).
+- [Prettier](https://prettier.io/).
+- [VSCode](https://code.visualstudio.com/) settings.
+
+## Dev
+
+```bash
+yarn # to install
+yarn dev # to dev
+yarn build # to build
+yarn preview # to preview/serve (need to build first)
+yarn prettier # to prettier
+yarn lint # to lint
+```
+
+💡 Install [Preview.js](https://previewjs.com/) to quickly preview running app in VSCode. No need to run the server!
--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,6 @@
+import React from 'react'
+import { FcIdea } from 'react-icons/fc'
+
 import './App.css'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -5,19 +8,27 @@ import viteLogo from '/vite.svg'
 function App() {
   return (
     <div className="h-screen w-screen">
-      <div className="h-full w-11/12 flex flex-col items-center justify-center bg-white">
-        <div className="flex flex-col gap-8 items-center justify-center">
-          <div className="flex gap-4 h-12 items-center justify-center w-full">
-            <img src={viteLogo} className="logo h-full animate-bounce" alt="Vite logo" />
-            <img src={reactLogo} className="logo react h-full animate-spin" alt="React logo" />
+      <div className="flex h-full w-11/12 flex-col items-center justify-center bg-white">
+        <div className="flex flex-col items-center justify-center gap-8">
+          <div className="flex h-12 w-full items-center justify-center gap-4">
+            <img src={viteLogo} className="h-full animate-bounce" alt="Vite logo" />
+            <img src={reactLogo} className="h-full animate-spin" alt="React logo" />
           </div>
 
-          <div className="flex items-center justify-center flex-col gap-4">
-            <h1 className="text-4xl text-slate-800">Vite React TS Starter</h1>
-            <h2 className="text-xl text-slate-600">Created by Anh-Thi Dinh</h2>
+          <div className="flex flex-col items-center justify-center gap-4">
+            <h1 className="text-3xl font-semibold text-slate-800">
+              Vite 🤝 React 🤝 TypeScript 🤝 Tailwind 🤝 React Icons
+            </h1>
+            <h2 className="text-xl text-slate-700">Created by Anh-Thi Dinh</h2>
           </div>
 
-          <p></p>
+          <div className="flex items-center justify-center gap-3 rounded-lg bg-sky-100 py-2 px-4 text-sky-900 shadow-xl">
+            <FcIdea className="text-2xl" />
+            <div>
+              Make components inside <code className="text-pink-700">src/components/</code> for
+              testing.
+            </div>
+          </div>
         </div>
       </div>
     </div>
