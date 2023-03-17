27fe0f2963ae2f22d2ac8bc92faaa079328b404f
Thi <thi@ideta.io>
2023-03-17 11:34:31 +0100

    Create placeholder

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,10 @@
 import React from 'react'
-import { FcIdea } from 'react-icons/fc'
 
 import './App.css'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import PlaceHolder from './components/placeholder'
 
 function App() {
-  return (
-    <div className="h-screen w-screen">
-      <div className="flex h-full w-11/12 flex-col items-center justify-center bg-white">
-        <div className="flex flex-col items-center justify-center gap-8">
-          <div className="flex h-12 w-full items-center justify-center gap-4">
-            <img src={viteLogo} className="h-full animate-bounce" alt="Vite logo" />
-            <img src={reactLogo} className="h-full animate-spin" alt="React logo" />
-          </div>
-
-          <div className="flex flex-col items-center justify-center gap-4">
-            <h1 className="text-3xl font-semibold text-slate-800">
-              Vite 🤝 React 🤝 TypeScript 🤝 Tailwind 🤝 React Icons
-            </h1>
-            <h2 className="text-xl text-slate-700">Created by Anh-Thi Dinh</h2>
-          </div>
-
-          <div className="flex items-center justify-center gap-3 rounded-lg bg-sky-100 py-2 px-4 text-sky-900 shadow-xl">
-            <FcIdea className="text-2xl" />
-            <div>
-              Make components inside <code className="text-pink-700">src/components/</code> for
-              testing.
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  )
+  return <PlaceHolder />
 }
 
 export default App
---
+++ src/components/placeholder.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { FcIdea } from 'react-icons/fc'
+
+import viteLogo from '../../public/vite.svg'
+import reactLogo from '../assets/react.svg'
+
+export default function PlaceHolder() {
+  return (
+    <div className="h-screen w-screen">
+      <div className="flex h-full w-11/12 flex-col items-center justify-center bg-white">
+        <div className="flex flex-col items-center justify-center gap-8">
+          <div className="flex h-12 w-full items-center justify-center gap-4">
+            <img src={viteLogo} className="h-full animate-bounce" alt="Vite logo" />
+            <img src={reactLogo} className="h-full animate-spin" alt="React logo" />
+          </div>
+
+          <div className="flex flex-col items-center justify-center gap-4">
+            <h1 className="text-3xl font-semibold text-slate-800">
+              Vite 🤝 React 🤝 TypeScript 🤝 Tailwind 🤝 React Icons
+            </h1>
+            <h2 className="text-xl text-slate-700">Created by Anh-Thi Dinh</h2>
+          </div>
+
+          <div className="flex items-center justify-center gap-3 rounded-lg bg-sky-100 py-2 px-4 text-sky-900 shadow-xl">
+            <FcIdea className="text-2xl" />
+            <div>
+              Put your components inside <code className="text-pink-700">src/components/</code> and
+              replace <code className="text-pink-700">PlaceHolder</code> with yours.
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
